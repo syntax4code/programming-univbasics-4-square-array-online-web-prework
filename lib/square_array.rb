02af7d4a1c7ea39c3de55ceedba8b57d2e4e5099
Warren Loy Harper II <warrenharper2@att.net>
2019-07-22 00:20:02 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+   counter = 0
+  while numbers[counter] do
+    puts numbers[counter]
+    counter += 1
+  end
 end
+numbers = [1,2,3]
+new_numbers = [{numbers.map { |numbers| numbers * numbers }]
+square_array(new_numbers)
